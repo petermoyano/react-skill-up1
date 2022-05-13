0bdb3f9d758e5cb4668078d8eeeeaf738923a2d7
Pedro Moyano <pedromoyano454@gmail.com>
2022-05-13 15:49:11 -0300

    Movies maping and uuid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ function Footer (){
     return(<footer>
         <nav>
             <ul>
-                <li><a href="twitter.com" rel="noopener noreferrer">twitter</a></li>
-                <li><a href="twitter.com" rel="noopener noreferrer">FB</a></li>
-                <li><a href="twitter.com" rel="noopener noreferrer">IG</a></li>
+                <li><a href="twitter.com" rel="noopener noreferrer"></a>twitter</li>
+                <li><a href="twitter.com" rel="noopener noreferrer"></a>FB</li>
+                <li><a href="twitter.com" rel="noopener noreferrer"></a>IG</li>
             </ul>
             <p>All rigths reserved. Copyright Alkemy challenge</p>
         </nav>
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,22 +1,29 @@
-import axios from "axios";
-import { useEffect } from "react";
+// React and RR imports
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
+// external libraries
+import { v4 as uuidv4 } from 'uuid';
+import axios from "axios";
+
+//components
+import MovieCard from "./MovieCard"
+
+
 const Listado = () => {
+    const [movieList, setMovieList] = useState([])
     const API_KEY = "4245f97181b9770be8a62c794e0c582e"
     const token = localStorage.getItem("Alkemytoken")
-    console.log(token);
 
-useEffect(()=> {
-    const ENDPOINT = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
-    async function fetchData(){
-    const response = await axios.get(ENDPOINT);
-    console.log(response.data)
-    }
-    fetchData();
-    
 
-}, [])
+    useEffect(() => {
+        const ENDPOINT = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`;
+        async function fetchData() {
+            const response = await axios.get(ENDPOINT);
+            setMovieList(response.data.results);
+        }
+        fetchData();
+    }, [setMovieList])
 
 
 
@@ -24,8 +31,14 @@ useEffect(()=> {
 
     return <>
         {!token && <Navigate replace to="/" />}
-            <h2>Soy el componente listado</h2>
-        </>
-        }
+        <h2>Soy el componente listado</h2>
+        <ul>
+        {movieList.map((movie => {
+            return <MovieCard key={uuidv4()} movie={movie} />
 
-        export default Listado;
+        }))}
+        </ul>
+    </>
+}
+
+export default Listado;
