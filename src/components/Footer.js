dad66d6d4fc30d6cd26d296f342ea4d2722b35b6
Pedro Moyano <pedromoyano454@gmail.com>
2022-06-15 16:55:53 -0300

    Basic Footer styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,72 @@
 import React from "react";
+import { Box, Container, Grid } from "@mui/material";
+import { Link } from "react-router-dom"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 
-function Footer (){
-    return(<footer>
-        <nav>
-            <ul>
-                <li><a href="twitter.com" rel="noopener noreferrer"></a>twitter</li>
-                <li><a href="twitter.com" rel="noopener noreferrer"></a>FB</li>
-                <li><a href="twitter.com" rel="noopener noreferrer"></a>IG</li>
-            </ul>
-            <p>All rigths reserved. Copyright Alkemy challenge</p>
-        </nav>
+function Footer() {
+    return (<footer>
+        <Box
+            bgcolor={"text.primary"}
+            color="teal"
+            px={{ xs: 3, sm: 10 }}
+            py={{ xs: 5, sm: 10 }}
+        >
+            <Container maxWidth="lg">
+                <Grid container spacing={5}>
+                    <Grid item xs={12} sm={4}>
+                        <Box borderBottom={1} sx={{ paddingBottom: 1 }}>
+                            Go to
+                        </Box>
+                        <Box sx={{ p: 1 }}>
+                            <Link to="/contact" color="inherit">
+                                About
+                            </Link>
+                        </Box>
+                        <Box sx={{ p: 1 }}>
+                            <Link to="/privacy" color="inherit">
+                                Back end
+                            </Link>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Box borderBottom={1}>
+                            Account
+                        </Box>
+                        <Box sx={{ p: 1 }}>
+                            <Link to="/" color="inherit">
+                                Login
+                            </Link>
+                        </Box>
+                        <Box sx={{ p: 1 }}>
+                            <Link to="/" color="inherit">
+                                Register
+                            </Link>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                        <Box borderBottom={1}>
+                            Contact
+                        </Box>
+                        <Grid container justifyContent={"space-evenly"} sx={{ py: 2 }}>
+                            <Link to="/" color="inherit" >
+                                <GitHubIcon />
+                            </Link>
+                            <Link to="/" color="inherit">
+                                <TwitterIcon />
+                            </Link>
+                            <Link to="/" color="inherit">
+                                <LinkedInIcon />
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Box pt={{ xs: 5, sm: 10 }} pb={{ xs: 5, sm: 0 }}>The boring App &reg; {new Date().getFullYear()}</Box>
+        </Box>
+
     </footer>)
 }
 
